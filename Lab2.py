3b9ef27336df4a3c10fd594472e9d4970c45a24f
OngJinHoe <JINHOE.22@ichat.sp.edu.sg>
2023-10-23 16:51:51 +0800

    Version 2

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,34 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    display_main_menu()
+    num_list = get_user_input()
+    calc_average_temperature(num_list)
+    calc_min_max_temperature(num_list)
+
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5,67,32)")
+
+
+def get_user_input():
+    Inputs = input()
+    FloatList = list(map(float, Inputs.split(",")))
+    return FloatList
+
+
+def calc_average_temperature(ListOfNumber):
+    sum = 0
+    for x in ListOfNumber:
+        sum += x
+    average = sum/len(ListOfNumber)
+    print("Average = " + str(average))
+    return average
+
+def calc_min_max_temperature(ListOfNumber):
+    Minimum = min(ListOfNumber)
+    Maximum = max(ListOfNumber)
+    print("Maximum = " + str(Maximum))
+    print("Minimum = " + str(Minimum))
+    return Maximum,Minimum
+
+if __name__ == "__main__":
+    main()

399f39485efd7d996382dbcb21bec54e6938cafb
OngJinHoe <JINHOE.22@ichat.sp.edu.sg>
2023-10-30 15:08:07 +0800

    modified v1

--- bmi.py
+++ bmi.py
@@ -5,9 +5,13 @@ def calculate_bmi(height, weight):
     print("BMI = " + str(bmi))
     if bmi < 18.5:
         print("Underweight")
+        return -1
     elif 18.5 <= bmi <= 25.0:
         print("Normal weight")
+        return 0
     else:
         print("Overweight")
+        return 1
 
-calculate_bmi(weight=57, height=1.73)
+value = calculate_bmi(weight=57, height=1.73)
+print(value)
